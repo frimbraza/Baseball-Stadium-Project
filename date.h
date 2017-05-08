4b86bd8c5b9b2ffb6827addc833fcb7bad8cfd96
frimbraza <ha.kit.chan@gmail.com>
2017-05-07 17:42:10 -0700

    Add files via upload

---
+++ date.h
@@ -0,0 +1,9 @@
+#ifndef DATE_H
+#define DATE_H
+
+class Date
+{
+
+};
+
+#endif // DATE_H
---
+++ stadiuminfo.h
@@ -0,0 +1,49 @@
+#ifndef STADIUMINFO_H
+#define STADIUMINFO_H
+
+#include "date.h"
+
+
+class Stadium
+{
+private:
+    // Info gathered in the docs
+    string name;
+    string team;
+    string street;
+    string cityInfo;
+    string phoneNumber;
+    int capacity;
+
+    // Info outside file
+    Date opened;
+    bool grass;
+    bool NL;
+
+public:
+    Stadium();
+
+    void setName(string newName);
+    void setTeam(string newTeam);
+    void setStreet(string newStreet);
+    void setCityInfo(string newCity);
+    void setPhone(string newPhone);
+    void setCap(int newCap);
+    void setOpened(Date newOpen);
+    void setHasGrass(bool newHasGrass);
+    void setIsNL(bool newIsNL);
+
+    string getName();
+    string getTeam();
+    string getStreet();
+    string getCityInfo();
+    string getPhone();
+    int getCapacity();
+    Date getOpened();
+    bool hasGrass();
+    bool isNL();
+
+};
+
+
+#endif // STADIUMINFO_H
