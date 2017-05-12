e65a0eececb835263ea73b4010fa5f5af455a425
frimbraza <ha.kit.chan@gmail.com>
2017-05-12 08:37:52 -0700

    Add files via upload

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,15 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "binarytree.h"
+#include "stadiuminfo.h"
+
+struct Souvenir
+{
+private:
+    string name;
+    double price;
+};
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +23,20 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    // Fix these up to be compatible with the GUI
+    BinaryTree<StadiumInfo> sortByTeamName();
+    BinaryTree<StadiumInfo> sortByStadiumName();
+    BinaryTree<StadiumInfo> sortByDate();
+
+    void grassList();
+    void alList();
+    void nlList();
 
 private:
     Ui::MainWindow *ui;
+    BinaryTree<StadiumInfo> stadiumList;
+
+
 };
 
 #endif // MAINWINDOW_H
