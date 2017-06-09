e53a3c1141de8285bf7d8d2ab428338d7b54e397
NaregHov <nareghov@gmail.com>
2017-06-09 11:23:12 -0700

    Create date.h

--- BaseballStadiumPlanner/date.h
+++ BaseballStadiumPlanner/date.h
@@ -1,3 +1,12 @@
+/*date header file
+* 
+* date assigns months to numbers and converts strings to ints and vice versa
+* 
+* initializes strings and print functions
+* 
+*/
+
+
 #ifndef DATE_H
 #define DATE_H
 #include <iostream>
