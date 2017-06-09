46d833dc7ab675ee0222e077d02cef3143e363c2
prestonc26 <prestonc26@gmail.com>
2017-06-09 01:53:38 -0700

    Create mainwindow.cpp

--- BaseballStadiumPlanner/mainwindow.cpp
+++ BaseballStadiumPlanner/mainwindow.cpp
@@ -22,12 +22,14 @@ MainWindow::MainWindow(QWidget *parent) :
     addSouvenirList();
 }
 
+//deconstruct ui
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
 //skips hidden 3 characters at beginning of text file
+//O(1)
 void MainWindow::SkipBOM(std::ifstream &in)         // Added 6/7/2017
 {
     char test[3] = {0};
@@ -41,6 +43,8 @@ void MainWindow::SkipBOM(std::ifstream &in)         // Added 6/7/2017
     in.seekg(0);
 }
 
+//intializes souvenir data into binary tree of souvenir class
+//O(n)
 void MainWindow::addSouvenirList()
 {
     ifstream inFile;
@@ -74,6 +78,8 @@ void MainWindow::addSouvenirList()
 
 }
 
+//intialize stadium data into binary tree of class stadiumInfo
+//O(n)
 void MainWindow::readFromFile()
 {
     ifstream inFile;
@@ -146,6 +152,9 @@ void MainWindow::readFromFile()
     // This is where the output is coming from. Take out later
     // stadiumList->displayInOrder();
 }
+
+//pass stadium info into listwindow ui
+//O(n)
 void MainWindow::on_ListButton_clicked()
 {
     //ListWindow listWindow;
@@ -179,6 +188,8 @@ void MainWindow::on_ListButton_clicked()
 }
 
 // 6/8/2017!!!!!!!!!!!!!!!!!!!!!!!!!!
+//pass stadium info into trip window
+//O(n)
 void MainWindow::on_TripsButton_clicked()
 {
     tripWindow = new TripWindow(this);
@@ -201,6 +212,8 @@ void MainWindow::on_TripsButton_clicked()
     tripWindow->exec();
 }
 
+//open admin window
+//O(1)
 void MainWindow::on_AdminButton_clicked()
 {
     AdminLogin = new adminLogin(this);
@@ -210,6 +223,8 @@ void MainWindow::on_AdminButton_clicked()
 }
 
 
+//reintializes all binary trees using new data outfiled while in other ui windows
+//O(n)
 void MainWindow::on_UpdateButton_clicked()
 {
     delete stadiumList;
